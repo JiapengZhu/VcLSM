993dc3a9a32adb4195e3501888e9dd8c55afbba1
Valkryst <valkryst@valkryst.com>
2017-04-07 15:14:33 -0300

    Initial commit.

---
+++ src/main/com/valkryst/VcLSM/NodeInstrumentation.java
@@ -0,0 +1,35 @@
+package main.com.valkryst.VcLSM;
+
+import java.lang.instrument.Instrumentation;
+
+public class NodeInstrumentation {
+    /** Handle to instance of the Instrumentation interface. */
+    private static Instrumentation instrumentation;
+
+    /**
+     * Implementation of the overloaded premain method that is first invoked by
+     * the JVM during use of instrumentation.
+     *
+     * @param args
+     *         Agent options provided as a single String.
+     *
+     * @param inst
+     *         Handle to instance of Instrumentation provided on command-line.
+     */
+    public static void agentmain(final String args, final Instrumentation inst) {
+        instrumentation = inst;
+    }
+
+    /**
+     * Estimates the size of the specified node.
+     *
+     * @param node
+     *         The node.
+     *
+     * @return
+     *         The estimated size of the node.
+     */
+    public static long getNodeSize(final Node node) {
+        return instrumentation.getObjectSize(node);
+    }
+}
