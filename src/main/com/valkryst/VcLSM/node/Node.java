cbb80838e8c461bff1b5d0aaf8ef1ba94160d744
Valkryst <valkryst@valkryst.com>
2017-04-14 18:40:05 -0300

    Made alterations to the equals method and added check for null argument.

--- src/main/com/valkryst/VcLSM/node/Node.java
+++ src/main/com/valkryst/VcLSM/node/Node.java
@@ -39,17 +39,17 @@ public class Node <V> implements Comparable<Node<V>> {
 
     @Override
     public boolean equals(final Object obj) {
-        if (obj instanceof Node) {
-            final Node<V> otherNode = (Node<V>) obj;
-
-            boolean isEqual = key.equals(otherNode.getKey());
-            isEqual &= time.equals(otherNode.getTime());
-            isEqual &= value.equals(otherNode.getValue());
-
-            return isEqual;
-        } else {
+        if (obj == null || ! (obj instanceof Node)) {
             return false;
         }
+
+        final Node<V> otherNode = (Node<V>) obj;
+
+        boolean isEqual = key.equals(otherNode.getKey());
+        isEqual &= time.equals(otherNode.getTime());
+        isEqual &= value.equals(otherNode.getValue());
+
+        return isEqual;
     }
 
     /** @return The key concatenated with the timestamp. */
