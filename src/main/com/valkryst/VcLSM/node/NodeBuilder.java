f5434de31e9082199a5a2f084e2bcd81c3d1ea02
Valkryst <valkryst@valkryst.com>
2017-04-15 15:31:09 -0300

    The creation time of a node is now handled by the NodeBuilder.
    Partially implemented a method to read a node from JSON.

--- src/main/com/valkryst/VcLSM/node/NodeBuilder.java
+++ src/main/com/valkryst/VcLSM/node/NodeBuilder.java
@@ -1,10 +1,16 @@
 package main.com.valkryst.VcLSM.node;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import lombok.Getter;
+import main.com.valkryst.VcLSM.C;
+
+import java.time.LocalDateTime;
 
 public class NodeBuilder <V> {
     /** The key. */
     @Getter private String key = null;
+    /** The date & time at which the node was created. */
+    @Getter private LocalDateTime time = LocalDateTime.now();
     /** The value. */
     @Getter private V value = null;
 
@@ -37,6 +43,10 @@ public class NodeBuilder <V> {
             throw new IllegalStateException("A Node cannot have an empty key.");
         }
 
+        if (time == null) {
+            time = LocalDateTime.now();
+        }
+
         if (value == null) {
             throw new IllegalStateException("A Node cannot have a null value.");
         }
@@ -48,8 +58,23 @@ public class NodeBuilder <V> {
         }
     }
 
+    public void loadFromJSON(final JsonNode object) {
+        final String keyTmp = object.get("key").asText();
+        final String timeTmp = object.get("time").asText();
+        final String valueTmp = object.get("value").asText();
+
+        this.key = keyTmp;
+        this.time = LocalDateTime.parse(timeTmp, C.FORMATTER);
+        // todo Finish
+    }
+
     public NodeBuilder<V> setKey(final String key) {
         this.key = key;
+        return this;
+    }
+
+    public NodeBuilder<V> setTime(final LocalDateTime time) {
+        this.time = time;
         return this;
     }
 
